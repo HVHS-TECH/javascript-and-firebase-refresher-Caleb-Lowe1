956a6b5ce2489281d74be91d292151102e97926f
Caleb-Lowe1 <22026cl@hvhs.school.nz>
2026-02-10 09:26:21 +1300

    version 5 changed some stuff in script.mjs

--- script.js
+++
@@ -1,6 +0,0 @@
-var messageSpace = getElementbyId("welcomeMessage");
-messageSpace.innerHTML = "You've connected to the JavaScript!";
-
-function start() {
-spaceForJavaScriptOutput.innerHTML
-}
---
+++ script.mjs
@@ -0,0 +1,8 @@
+
+
+
+function start() {
+spaceForJavaScriptOutput.innerHTML
+var messageSpace = getElementbyId("welcomeMessage");
+messageSpace.innerHTML = "You've connected to the JavaScript!";
+}
