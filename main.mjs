3f54ecafd53cf77c73c91a04a3777865a89c2f31
Caleb-Lowe1 <22026cl@hvhs.school.nz>
2026-02-11 12:18:30 +1300

    version 9, issues hopefully fixed

---
+++ main.mjs
@@ -0,0 +1,5 @@
+function start() {
+
+buttonPressed.innerHTML = "you pressed the button"
+messageSpace.innerHTML = "You've connected to the JavaScript!";
+}
--- script.mjs
+++ script.mjs
@@ -1,11 +1,7 @@
 
 
 
-function start() {
 
-buttonPressed.innerHTML = "you pressed the button"
-messageSpace.innerHTML = "You've connected to the JavaScript!";
-}
 /**************************************************************/
 // main.mjs
 // Main entry for index.html
