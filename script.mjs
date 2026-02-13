5b38fb3eb999612874512b799be3d77faf7a8475
Caleb-Lowe1 <22026cl@hvhs.school.nz>
2026-02-13 14:23:29 +1300

    Version 14 got initialise and authenticate working again, write is still broken though

--- script.mjs
+++ script.mjs
@@ -18,7 +18,7 @@ console.log('%c main.mjs',
 // Import all the constants & functions required from fb_io module
 
 import { fb_initialise, fb_authenticate, fb_WriteRec, fb_WriteRecPrivate}
-    from './fb.mjs';
+    from './fb_io.mjs';
     window.fb_initialise = fb_initialise;
     window.fb_authenticate = fb_authenticate;
     window.fb_WriteRec = fb_WriteRec;
